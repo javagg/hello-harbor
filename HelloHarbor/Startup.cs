f5a269fde4f455c769ef7981035ba50d6f99067b
alexlee <lu.lee05@gmail.com>
2015-06-24 19:13:36 +0800

    work with iis

---
+++ HelloHarbor/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(HelloHarbor.Startup))]
+
+namespace HelloHarbor
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseWelcomePage();
+        }
+    }
+}
